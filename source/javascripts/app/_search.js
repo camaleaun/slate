c2ae5bdeeccd4681975bbbd2dfac7d975fa8bc15
Gilberto Tavares <gilberto@domum.com>
2015-08-05 12:10:59 -0300

    JSLint Problems resolved

--- source/javascripts/app/_search.js
+++ source/javascripts/app/_search.js
@@ -1,25 +1,23 @@
 //= require ../lib/_lunr
 //= require ../lib/_jquery.highlight
+/*globals lunr,$*/
 (function () {
   'use strict';
 
-  var content, searchResults;
-  var highlightOpts = { element: 'span', className: 'search-highlight' };
+  var content, searchResults,
+    highlightOpts = { element: 'span', className: 'search-highlight' },
 
-  var index = new lunr.Index();
+    index = new lunr.Index();
 
   index.ref('id');
   index.field('title', { boost: 10 });
   index.field('body');
   index.pipeline.add(lunr.trimmer, lunr.stopWordFilter);
 
-  $(populate);
-  $(bind);
-
   function populate() {
-    $('h1, h2').each(function() {
-      var title = $(this);
-      var body = title.nextUntil('h1, h2');
+    $('h1, h2').each(function () {
+      var title = $(this),
+        body = title.nextUntil('h1, h2');
       index.add({
         id: title.prop('id'),
         title: title.text(),
@@ -28,22 +26,26 @@
     });
   }
 
-  function bind() {
-    content = $('.content');
-    searchResults = $('.search-results');
+  function unhighlight() {
+    content.unhighlight(highlightOpts);
+  }
 
-    $('#input-search').on('keyup', search);
+  function highlight(value) {
+    if (value) { content.highlight(value, highlightOpts); }
   }
 
   function search(event) {
+    var input = event.currentTarget,
+      results;
+    
     unhighlight();
     searchResults.addClass('visible');
 
     // ESC clears the field
-    if (event.keyCode === 27) this.value = '';
+    if (event.keyCode === 27) { input.value = ''; }
 
-    if (this.value) {
-      var results = index.search(this.value).filter(function(r) {
+    if (input.value) {
+      results = index.search(input.value).filter(function (r) {
         return r.score > 0.0001;
       });
 
@@ -53,10 +55,10 @@
           var elem = document.getElementById(result.ref);
           searchResults.append("<li><a href='#" + result.ref + "'>" + $(elem).text() + "</a></li>");
         });
-        highlight.call(this);
+        highlight.call(input.value);
       } else {
         searchResults.html('<li></li>');
-        $('.search-results li').text('No Results Found for "' + this.value + '"');
+        $('.search-results li').text('No Results Found for "' + input.value + '"');
       }
     } else {
       unhighlight();
@@ -64,11 +66,13 @@
     }
   }
 
-  function highlight() {
-    if (this.value) content.highlight(this.value, highlightOpts);
+  function bind() {
+    content = $('.content');
+    searchResults = $('.search-results');
+
+    $('#input-search').on('keyup', function (e) { search(e); });
   }
 
-  function unhighlight() {
-    content.unhighlight(highlightOpts);
-  }
-})();
+  $(populate);
+  $(bind);
+}());
